20e55507dc5d7f768c1b90012b4414c4bf1e8b57
Abdelhak MEKAOUI <mekaouiabdelhak1@gmail.com>
2024-04-18 11:51:11 +0100

    configuring keycloak with access roles

---
+++ src/main/java/com/example/main/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.main.config;
+
+import com.example.main.services.JwtAuthConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
+
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated();
+
+        http
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS);
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/example/main/controllers/Demo.java
@@ -0,0 +1,40 @@
+package com.example.main.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class Demo {
+
+    @GetMapping
+    @PreAuthorize("hasRole('client_user')")
+    public String hello() {
+        return "Hello from Spring boot & Keycloak";
+    }
+
+    @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('client_admin')")
+    public String hello2() {
+        return "Hello from Spring boot & Keycloak - ADMIN";
+    }
+
+    @GetMapping("/userinfo")
+    @PreAuthorize("hasRole('admin_user')")
+    public String getUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            return "Authenticated user: " + username;
+        } else {
+            return "User not authenticated";
+        }
+    }
+}
